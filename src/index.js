5212f59bc4beadf306d033e14fbf5f88f1c68ae1
NamKhanhIT2 <khanha1k59@gmail.com>
2025-05-01 16:16:27 +0700

    done

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 const path=require('path')
 const express=require('express')
- const morgan = require('morgan');
+const morgan = require('morgan');
 const {engine}=require('express-handlebars')
- const app = express();
- const port = 3000;
+const methodOverride=require('method-override');
+const route=require('./routers')
+const db=require('./config/db');
+db.connect();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+const port = 3000;
  
  
  
+app.use(express.static(path.join(__dirname,'public')))
+app.use(morgan('combined'))
+app.engine('.hbs',engine({extname:'.hbs',
+    helpers:{
+        sum:(a,b)=>a+b,
+    }
+}))
+app.set('view engine','.hbs')
+app.set('views',path.join(__dirname,'resources','views'))
  
- app.use(express.static(path.join(__dirname,'public')))
- app.use(morgan('combined'))
- app.engine('.hbs',engine({extname:'.hbs'}))
- app.set('view engine','.hbs')
- app.set('views',path.join(__dirname,'resources','views'))
-
- app.get('/', (req, res) => {
-     res.render('home');
- })
- app.get('/news', (req, res) => {
-    res.render('news');
-})
+route(app);
  
  
  
